610ba8bceebb2dbf6f4a1521b5817182118a8be9
Grace-McNamara <grace.mcnamara@forcepoint.com>
2021-03-31 18:01:26 +0100

    Lambda refactoring

--- encryption_checker_lambda/lambda_function.py
+++ encryption_checker_lambda/lambda_function.py
@@ -10,22 +10,16 @@ client = boto3.client('s3')
 
 
 def lambda_handler(event: Dict[str, any], context):
+        if buckets_specified_in(event):
+            bucket_encryption_status = get_specified_bucket_status(buckets=event["Buckets"])
+        else:
+            buckets = get_all_buckets()
+            bucket_encryption_status = get_all_buckets_status(buckets=buckets)
 
-    bucket_encryption_status = {"buckets": {}}
-    if buckets_specified_in(event):
-        bucket_encryption_status = get_specified_bucket_status(buckets=event["buckets"])
-    else:
-        buckets = client.list_buckets()
-        print(buckets)
-        print(buckets)
-        for bucket in buckets['Buckets']:
-            result = retrieve_encryption_status(bucket['Name'])
-            bucket_encryption_status['buckets'][bucket['Name']] = result
-
-    return {
-        'statusCode': 200,
-        'body': bucket_encryption_status
-    }
+        return {
+            'statusCode': 200,
+            'body': bucket_encryption_status
+        }
 
 
 def buckets_specified_in(event: Dict[str, str]) -> bool:
@@ -35,7 +29,7 @@ def buckets_specified_in(event: Dict[str, str]) -> bool:
     :param event: The event object to check for buckets on.
     :return: True if buckets key exists and has one or more bucket names specified, else False.
     """
-    return 'buckets' in event and len(event['buckets']) > 0
+    return 'Buckets' in event and len(event['Buckets']) > 0
 
 
 def get_specified_bucket_status(buckets: List[str]) -> Dict[str, Dict[str, str]]:
@@ -45,20 +39,44 @@ def get_specified_bucket_status(buckets: List[str]) -> Dict[str, Dict[str, str]]
     :param buckets: A list of buckets to get the status of.
     :return: A dictionary containing the bucket names and their encryption status.
     """
-    bucket_encryption_status = {"buckets": {}}
+    bucket_encryption_status = {"Buckets": {}}
 
     for bucket in buckets:
         bucket_name = bucket
         result = retrieve_encryption_status(bucket_name)
-        bucket_encryption_status['buckets'][bucket_name] = result
+        if result is None:
+            bucket_encryption_status['Buckets'][bucket_name] = "Bucket doesn't exist"
+        else:
+            bucket_encryption_status['Buckets'][bucket_name] = result
 
     return bucket_encryption_status
 
 
-def retrieve_encryption_status(bucket_name):
+def retrieve_encryption_status(bucket_name) -> str:
+    """
+    Checks the encryption status for a given bucket name.
+
+    :param bucket_name: The bucket name to query the encryption of.
+    :return: The bucket's encryption status ["Encrypted"|"Not Encrypted"].
+    """
     try:
         client.get_bucket_encryption(Bucket=bucket_name)
-        return ("Encrypted")
+        return "Encrypted"
     except ClientError as e:
         if e.response.get('Error').get('Code') == 'ServerSideEncryptionConfigurationNotFoundError':
-            return ("Not Encrypted")
+            return "Not Encrypted"
+
+
+def get_all_buckets() -> Dict[str, str]:
+    """
+    Returns a dictionary of all S3 buckets on the AWS service account.
+    """
+    return client.list_buckets()
+
+
+def get_all_buckets_status(buckets):
+    bucket_encryption_status = {"Buckets": {}}
+    for bucket in buckets['Buckets']:
+        result = retrieve_encryption_status(bucket['Name'])
+        bucket_encryption_status['Buckets'][bucket['Name']] = result
+    return bucket_encryption_status
